73f912b76559d4115a71a9d70c25f57b75844c9f
GrumpyRock <rockleader+git@gmail.com>
2025-02-25 18:13:03 +0100

    Added calendar

--- parse.py
+++ parse.py
@@ -10,14 +10,15 @@ concert_list = soup.css.select('[itemtype="https://schema.org/MusicEvent"]')
 
 concerts = []
 
-for raw_concert in concert_list:
+for id_concert, raw_concert in enumerate(concert_list):
     concert = {}
+    concert['id'] = id_concert
     concert['location_name'] = raw_concert.select('[itemprop="location"] [itemprop="name"]')[0]['content'].replace("\\\\", "\\")
     concert['location_locality'] = raw_concert.select('[itemprop="location"] [itemprop="address"] [itemprop="addressLocality"]')[0]['content']
     concert['location_country'] = raw_concert.select('[itemprop="location"] [itemprop="address"] [itemprop="addressCountry"]')[0]['content']
     concert['name'] = raw_concert.select('[itemprop="name"]')[1]['content']
-    concert['start_date'] = raw_concert.select('[itemprop="startDate"]')[0]['content']
-    concert['end_date'] = raw_concert.select('[itemprop="endDate"]')[0]['content']
+    concert['start'] = raw_concert.select('[itemprop="startDate"]')[0]['content']
+    concert['end'] = raw_concert.select('[itemprop="endDate"]')[0]['content']
 
     concert['artists'] = []
 
@@ -30,7 +31,10 @@ html_concert_list = soup.css.select('tbody tr td:last-child')
 # A faire : vérifier que les éléments des deux listes correspondent
 for index, raw_html_concert in enumerate(html_concert_list):
     artists_html = raw_html_concert.select('div.col')
+    concerts[index]['title'] = ""
     for sub_html in artists_html:
         concerts[index]['artists'].append({ 'name': sub_html.b.text, 'genres': re.split("/|,|\\|", sub_html.i.text) })
+        concerts[index]['title'] += " + " if concerts[index]['title'] else " "
+        concerts[index]['title'] += sub_html.b.text
 
 print(json.dumps(concerts, ensure_ascii=False))
